9df3df7149f3ad7b75a5967182327e7440e566f9
yashiqxlabai <165651689+yashiqxlabai@users.noreply.github.com>
2024-06-14 13:03:13 +0530

    torch_dtype

--- scripts/run_sft.py
+++ scripts/run_sft.py
@@ -129,12 +129,12 @@ def main():
         model_args.torch_dtype if model_args.torch_dtype in ["auto", None] else getattr(torch, model_args.torch_dtype)
     )
     quantization_config = get_quantization_config(model_args)
-
+    print("\nTorch_Dtype"+ str(torch_dtype))
     model_kwargs = dict(
         revision=model_args.model_revision,
         trust_remote_code=model_args.trust_remote_code,
         use_flash_attention_2=model_args.use_flash_attention_2,
-        torch_dtype=torch_dtype,
+        torch_dtype=model_args.torch_dtype,
         use_cache=False if training_args.gradient_checkpointing else True,
         device_map= get_kbit_device_map() if quantization_config is not None else None,
         quantization_config=quantization_config,
